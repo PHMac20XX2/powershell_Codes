3230e715e17a2687f5f4246355dd82922a513afe
PHM <phmestudos@gmail.com>
2023-09-12 16:20:36 -0300

    scriptATT1

--- PHMscripts/s1.ps1
+++ PHMscripts/s1.ps1
@@ -4,24 +4,13 @@ $opcao = Read-Host "
 
 1. Adicionar Licenca
 2. Adicionar Licencas em Massa
-3. Remover licenca
-4. Remover licencas em Massa
 
 Escolha a opcao"
 
 if ($opcao = 1) {
     Write-Host "Adicao de Licenca"
 }
-if ($opcao = 2) {
+elseif ($opcao = 2) {
     Write-Host "Adicao em Massa"
-} 
-if ($opcao = 3) {
-    Write-Host "Remover Licenca"
 }
-if ($opcao = 4) {
-    Write-Host "Remocao em Massa"
-}
-else {
-    Write-Host "Opção Invalida"
-    exit 
-}
+
